313553f6b98668fdfb5e17ce3cab836903e04476
ZahiritaRuiz <125690093+ZahiritaRuiz@users.noreply.github.com>
2024-12-05 19:20:59 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,3 +9,28 @@ self.addEventListener('push', event => {
         self.registration.showNotification('Tienda de Mascotas', options)
     );
 });
+const CACHE_NAME = 'travel-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/script.js',
+  '/logo2-192x192.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
